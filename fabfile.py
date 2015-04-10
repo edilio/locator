a1553b254f2809a51e194769d7107deb9f784456
edilio <edilio73@gmail.com>
2015-04-10 14:29:02 -0400

    fabfile, static root and better requirements

---
+++ fabfile.py
@@ -0,0 +1,25 @@
+from fabric.api import run, env, task, cd, sudo
+from fabric.operations import get
+
+"""
+pip install fabric
+how to run:
+fab deploy -H ubuntu@www.jedutils.com
+"""
+
+
+ENV_NAME = 'locator'
+PORT = 8012
+
+
+def gen_unicorn_cmd():
+    return 'gunicorn -w 2 -b 127.0.0.1:{} -n {} locator.wsgi:application'.format(PORT, ENV_NAME)
+
+@task
+def deploy():
+    prob_home = '/var/www/django/locator'
+    with cd(prob_home):
+        sudo('git pull')
+        run('workon {} && python manage.py collectstatic --noinput'.format(ENV_NAME))
+        run('workon {} && python manage.py migrate'.format(ENV_NAME))
+        run('workon {} && {} &'.format(ENV_NAME, gen_unicorn_cmd()))
